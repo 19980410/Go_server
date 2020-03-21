ddaa8a8687df144465118991e0c96bab2bee0b3a
19980410 <kkaoru1998@gmail.com>
2020-03-21 23:20:00 +0900

    Goのサーバーが立ち上がっt あ

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main (){
+	fs := http.FileServer(http.Dir("static"))
+
+	http.Handle("/", fs)
+
+	log.Println("listening.........")
+	http.ListenAndServe(":3000", nil)
+}
+
